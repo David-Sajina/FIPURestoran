be10bdb7b909e22d48bc28f5cba3cb87969f31cd
kresimirspehar <kresimirspehar5@gmail.com>
2022-01-10 19:54:25 +0100

    meni proba

--- src/views/Meni.vue
+++ src/views/Meni.vue
@@ -1,13 +1,57 @@
 <template>
-  <div >
-    <h1>Meni</h1>
+  <div class="meni">
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-card class="text-xs-center ma-3" max-width="344">
+          <v-img
+            src="http://bhglas.dk/wp-content/uploads/2020/05/sarajevski-cevapi-PR.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Ćevapi </v-card-title>
+
+          <v-card-subtitle> 651 kcal </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="red" text> Naruči </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card class="text-xs-center ma-3" max-width="344">
+          <v-img
+            src="https://bigseventravel.com/wp-content/uploads/2019/06/Screenshot-2019-06-29-at-13.12.09.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Pizza </v-card-title>
+
+          <v-card-subtitle> 1700 kcal </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="red" text> Naruči </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-card class="text-xs-center ma-3" max-width="344">
+          <v-img
+            src="https://insanelygoodrecipes.com/wp-content/uploads/2020/10/Hamburger-with-Sesame-Seeds-Cheese-and-Veggies.png"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Hamburger </v-card-title>
+
+          <v-card-subtitle> 560 kcal </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="red" text> Naruči </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-layout>
+    </v-container>
   </div>
-  
 </template>
 <script>
 export default {
-    
-    name: 'Meni'
-  }
-
+  name: "Meni",
+};
 </script>
