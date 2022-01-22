b236906104f577123e9995de69408d355d78eea3
kresimirspehar <kresimirspehar5@gmail.com>
2022-01-22 12:55:21 +0100

    small update

--- src/views/Meni.vue
+++ src/views/Meni.vue
@@ -1,94 +1,93 @@
 <template>
-	<div class="meni">
-		<v-container class="my-5">
-			<v-layout row wrap>
-				<meni-card v-for="n in cards" :key="n" :info="n" />
-				<!-- Card za dodat -->
-				
+  <div class="meni">
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <meni-card v-for="n in cards" :key="n" :info="n" />
+        <!-- Card za dodat -->
 
-				<div class="meni" v-if="store.currentUser">
-					<v-container class="my-5">
-						<v-layout row wrap>
-							<v-card class="text-xs-center ma-3" max-width="344">
-								<v-col cols="auto">
-									<v-dialog>
-										<template v-slot:activator="{ on, attrs }">
-											<v-btn
-												color="primary"
-												height="315px"
-												width="324px"
-												v-bind="attrs"
-												v-on="on"
-												><h1>Dodaj novo jelo</h1></v-btn
-											>
-										</template>
-										<template v-slot:default="dialog">
-											<v-card>
-												<v-toolbar color="primary" dark
-													>Dodavanje novog jela</v-toolbar
-												>
-												<v-card-text>
-													<form
-														@submit.prevent="postNewImage"
-														class="form-inline mb-5"
-													>
-														<div class="form-group">
-															<label for="imageUrl">Image URL</label>
-															<input
-																v-model="newImageUrl"
-																type="text"
-																class="form-control ml-2"
-																placeholder="Enter the image URL"
-																id="imageUrl"
-															/>
-														</div>
-														<div class="form-group">
-															<label for="Title">Naslov</label>
-															<input
-																v-model="newTitle"
-																type="text"
-																class="form-control ml-2"
-																placeholder="Enter the image description"
-																id="Title"
-															/>
-														</div>
-														<div class="form-group">
-															<label for="Price">Cijena</label>
-															<input
-																v-model="newPrice"
-																type="text"
-																class="form-control ml-2"
-																placeholder="Unesite cijenu"
-																id="Price"
-															/>
-														</div>
-														<button type="submit" class="btn btn-primary ml-2">
-															Post image
-														</button>
-													</form>
-												</v-card-text>
-												<v-card-actions class="justify-end">
-													<v-btn text @click="dialog.value = false"
-														>Zatvori</v-btn
-													>
-												</v-card-actions>
-											</v-card>
-										</template>
-									</v-dialog>
-								</v-col>
-							</v-card>
-						</v-layout>
-					</v-container>
-				</div>
-			</v-layout>
-		</v-container>
-	</div>
+        <div class="meni" v-if="store.currentUser">
+          <v-container class="my-5">
+            <v-layout row wrap>
+              <v-card class="text-xs-center ma-3" max-width="344">
+                <v-col cols="auto">
+                  <v-dialog>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        color="primary"
+                        height="315px"
+                        width="324px"
+                        v-bind="attrs"
+                        v-on="on"
+                        ><h1>Dodaj novo jelo</h1></v-btn
+                      >
+                    </template>
+                    <template v-slot:default="dialog">
+                      <v-card>
+                        <v-toolbar color="primary" dark
+                          >Dodavanje novog jela</v-toolbar
+                        >
+                        <v-card-text>
+                          <form
+                            @submit.prevent="postNewImage"
+                            class="form-inline mb-5"
+                          >
+                            <div class="form-group">
+                              <label for="imageUrl">Image URL</label>
+                              <input
+                                v-model="newImageUrl"
+                                type="text"
+                                class="form-control ml-2"
+                                placeholder="Enter the image URL"
+                                id="imageUrl"
+                              />
+                            </div>
+                            <div class="form-group">
+                              <label for="Title">Naslov</label>
+                              <input
+                                v-model="newTitle"
+                                type="text"
+                                class="form-control ml-2"
+                                placeholder="Enter the image description"
+                                id="Title"
+                              />
+                            </div>
+                            <div class="form-group">
+                              <label for="Price">Cijena</label>
+                              <input
+                                v-model="newPrice"
+                                type="text"
+                                class="form-control ml-2"
+                                placeholder="Unesite cijenu"
+                                id="Price"
+                              />
+                            </div>
+                            <button type="submit" class="btn btn-primary ml-2">
+                              Post image
+                            </button>
+                          </form>
+                        </v-card-text>
+                        <v-card-actions class="justify-end">
+                          <v-btn text @click="dialog.value = false"
+                            >Zatvori</v-btn
+                          >
+                        </v-card-actions>
+                      </v-card>
+                    </template>
+                  </v-dialog>
+                </v-col>
+              </v-card>
+            </v-layout>
+          </v-container>
+        </div>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 <script>
-	import MeniCard from "@/components/MeniCard.vue";
-	import { firebase } from "@/firebase";
-	import { db } from "@/firebase";
-	import store from "@/store.js";
+import MeniCard from "@/components/MeniCard.vue";
+import { firebase } from "@/firebase";
+import { db } from "@/firebase";
+import store from "@/store.js";
 
 /* 	let cards = [
 		"https://insanelygoodrecipes.com/wp-content/uploads/2020/10/Hamburger-with-Sesame-Seeds-Cheese-and-Veggies.png",
@@ -96,39 +95,41 @@
 		"http://bhglas.dk/wp-content/uploads/2020/05/sarajevski-cevapi-PR.jpg",
 	];
  */
-	firebase.auth().onAuthStateChanged((user) => {
-		if (user) {
-			// User is signed in.
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in.
 
-			store.currentUser = user.email;
-		} else {
-			// User is not signed in.
+    store.currentUser = user.email;
+  } else {
+    // User is not signed in.
 
-			store.currentUser = null;
-		}
-	});
-	export default {
-		name: "Meni",
-		components: {
-			MeniCard,
-		},
-		data() {
-			return {
-        newTitle: "",
-        newImageUrl: "",
-        newPrice:"",
-				cards: [],
-				drawer: false,
-				store,
-			};
-		},
-    mounted(){
-      this.getMeni();
-    },
-		methods: {
-      getMeni(){
-        db.collection("jelo").get().then((query) => {
-          query.forEach((doc) =>{
+    store.currentUser = null;
+  }
+});
+export default {
+  name: "Meni",
+  components: {
+    MeniCard,
+  },
+  data() {
+    return {
+      newTitle: "",
+      newImageUrl: "",
+      newPrice: "",
+      cards: [],
+      drawer: false,
+      store,
+    };
+  },
+  mounted() {
+    this.getMeni();
+  },
+  methods: {
+    getMeni() {
+      db.collection("jelo")
+        .get()
+        .then((query) => {
+          query.forEach((doc) => {
             const dok = doc.data();
 
             this.cards.push({
@@ -136,39 +137,40 @@
               price: dok.Cijena,
               title: dok.desc,
               url: dok.url,
-            })
-          })
+            });
+          });
+        });
+    },
+    postNewImage() {
+      const imageUrl = this.newImageUrl;
+      const title = this.newTitle;
+      const price = this.newPrice;
+
+      db.collection("jelo")
+        .add({
+          url: imageUrl,
+          desc: title,
+          Cijena: price,
+          posted_at: Date.now(),
         })
-      },
-      postNewImage(){
-            const imageUrl = this.newImageUrl;
-            const title = this.newTitle;
-            const price = this.newPrice;
-            
-            db.collection("jelo").add({
-                url: imageUrl,
-                desc: title,
-                Cijena: price,
-                posted_at: Date.now(),
-            })
-            .then((doc)=>{
-                console.log("Spremljeno", doc)
-                this.newTitle = "";
-                this.newImageUrl = "";
-                this.newPrice = "";
-            })
-            .catch((e)=>{
-                console.error("greška")
-            })
-        },
-			logout() {
-				firebase
-					.auth()
-					.signOut()
-					.then(() => {
-						this.$router.push({ name: "Login" });
-					});
-			},
-		},
-	};
+        .then((doc) => {
+          console.log("Spremljeno", doc);
+          this.newTitle = "";
+          this.newImageUrl = "";
+          this.newPrice = "";
+        })
+        .catch((e) => {
+          console.error("greška");
+        });
+    },
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push({ name: "Login" });
+        });
+    },
+  },
+};
 </script>
