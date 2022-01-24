ace4cc81e5183947dd8a36edcb80dcf306380232
kresimirspehar <kresimirspehar5@gmail.com>
2022-01-24 16:59:39 +0100

    + dodavanje osoblja

--- src/views/Osoblje.vue
+++ src/views/Osoblje.vue
@@ -1,6 +1,16 @@
 <template>
-
+  <div>
+    <DodajOsoblje />
+  </div>
 </template>
-
 <script>
+import DodajOsoblje from "@/components/DodajOsoblje.vue";
+
+export default {
+  name: "Osoblje",
+  components: {
+    DodajOsoblje,
+  },
+};
 </script>
+<style></style>
