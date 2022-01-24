b016544467bd2d2d78d31d38757337b4e874ea68
kresimirspehar <kresimirspehar5@gmail.com>
2022-01-24 17:19:51 +0100

    komponenta osoblja

---
+++ src/components/DodajOsoblje.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="red lighten-1">
+                <v-toolbar-title>Dodaj osoblje</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    prepend-icon=""
+                    name="username"
+                    label="Email"
+                    type="email"
+                    v-model="username"
+                  ></v-text-field>
+                  <v-text-field
+                    id="password"
+                    prepend-icon=""
+                    name="password"
+                    label="Lozinka"
+                    type="password"
+                    v-model="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn dark color="red lighten-1" @click="dodaj"
+                  >Dodaj zaposlenika</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { firebase } from "@/firebase";
+
+export default {
+  name: "DodavanjeZaposlenika",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    dodaj() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.username, this.password)
+        .then(function () {
+          console.log("Uspješno dodavanje zaposlenika");
+        })
+        .catch(function (error) {
+          console.error("Došlo je do greške", error);
+        });
+      console.log("Nastavak");
+    },
+  },
+};
+</script>
+
+<style></style>
