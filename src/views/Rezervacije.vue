c385ce44b52398c962f981b3270391b0ef7be90d
kresimirspehar <kresimirspehar5@gmail.com>
2022-02-16 15:45:20 +0100

    Rezervacije.vue update

--- src/views/Rezervacije.vue
+++ src/views/Rezervacije.vue
@@ -1,3 +1,52 @@
-<template></template>
+<template>
+  <div>
+    <RezCard class="rez" v-for="n in cards" :key="n.id" :info="n" />
+  </div>
+</template>
 
-<script></script>
+<script>
+import RezCard from "@/components/RezCard.vue";
+import { db } from "@/firebase";
+import store from "@/store.js";
+
+export default {
+  name: "Rezervacije",
+  components: {
+    RezCard,
+  },
+  data() {
+    return {
+      cards: [],
+      store,
+    };
+  },
+  mounted() {
+    this.getPosts();
+  },
+  methods: {
+    getPosts() {
+      console.log("firebase dohvat...");
+
+      db.collection("rezervacije")
+        .get()
+        .then((query) => {
+          this.cards = [];
+          query.forEach((doc) => {
+            const data = doc.data();
+            console.log(data);
+
+            this.cards.push({
+              id: doc.id,
+              ime: data.ime,
+              broj: data.broj,
+              email: data.email,
+              datum: data.datum,
+              vrijeme: data.vrijeme,
+            });
+            console.log(this.cards);
+          });
+        });
+    },
+  },
+};
+</script>
