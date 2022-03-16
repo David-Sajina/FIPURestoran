eff97b3572a9e72a03329d3fcddc8f393c54b510
kresimirspehar <kresimirspehar5@gmail.com>
2022-03-16 15:05:19 +0100

    - reg pored logina

--- src/router/index.js
+++ src/router/index.js
@@ -6,105 +6,99 @@ import Home from "../views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-		meta: {
-		},
-	},
-	{
-		path: "/about",
-		name: "About",
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/About.vue"),
-	},
-	{
-		path: "/login",
-		name: "Login",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Login.vue"),
-      meta: {
-        needsUser: false,
-      },
-	},
-	{
-		path: "/rezerviraj",
-		name: "Rezerviraj",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Rezerviraj.vue"),
-	},
-	{
-		path: "/meni",
-		name: "meni",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Meni.vue"),
-	},
-	{
-		path: "/registracija",
-		name: "Registracija",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Registracija.vue"),
-	},
   {
-		path: "/narudzbe",
-		name: "Narudzbe",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Narudzbe.vue"),
-		meta: {
-			needsUser: true,
-		},
-	},
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {},
+  },
   {
-		path: "/rezervacije",
-		name: "Rezervacije",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Rezervacije.vue"),
-		meta: {
-			needsUser: true,
-		},
-	},
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
   {
-		path: "/osoblje",
-		name: "Osoblje",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Osoblje.vue"),
-		meta: {
-		needsUser: true,
-		},
-	},
-	{
-		path: "/checkout",
-		name: "Checkout",
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/Checkout.vue"),
-	},
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+    meta: {
+      needsUser: false,
+    },
+  },
+  {
+    path: "/rezerviraj",
+    name: "Rezerviraj",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Rezerviraj.vue"),
+  },
+  {
+    path: "/meni",
+    name: "meni",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Meni.vue"),
+  },
+
+  {
+    path: "/narudzbe",
+    name: "Narudzbe",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Narudzbe.vue"),
+    meta: {
+      needsUser: true,
+    },
+  },
+  {
+    path: "/rezervacije",
+    name: "Rezervacije",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Rezervacije.vue"),
+    meta: {
+      needsUser: true,
+    },
+  },
+  {
+    path: "/osoblje",
+    name: "Osoblje",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Osoblje.vue"),
+    meta: {
+      needsUser: true,
+    },
+  },
+  {
+    path: "/checkout",
+    name: "Checkout",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Checkout.vue"),
+  },
 ];
 
 const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
 router.beforeEach((to, from, next) => {
-	console.log(
-		"stara ruta je",
-		from.name,
-		"nova ruta je ",
-		to.name,
-		store.currentUser
-	);
+  console.log(
+    "stara ruta je",
+    from.name,
+    "nova ruta je ",
+    to.name,
+    store.currentUser
+  );
 
-	const noUser = store.currentUser == null;
+  const noUser = store.currentUser == null;
 
-  if (noUser && to.meta.needsUser){
-    console.error("NE DOPUSTAM")
-    next('Login');
+  if (noUser && to.meta.needsUser) {
+    console.error("NE DOPUSTAM");
+    next("Login");
+  } else {
+    next();
   }
-  else{
-	next();}
 });
 export default router;
