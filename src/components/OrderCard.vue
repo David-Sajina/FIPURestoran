00af5c2978fcd1cfb6dcfd4323608c116627beae
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2022-03-18 20:40:11 +0100

    +Narudzbe

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,76 @@
+<template>
+	<div>
+		<v-container style="margin-left: 20px" height="200px" class="my-4">
+			<v-layout row wrap>
+				<v-card class="text-xs-center ma-4" height="260" width="260">
+					<v-card-text>
+						<p class="text-h4 text--primary" style="text-align: center">
+							{{ info.posted_at }} 
+						</p>
+						<p></p>
+
+						<div class="text--primary" style="text-align:center;!important">
+							{{ info.stol }}<br />
+						</div>
+					</v-card-text>
+					      <v-col cols="auto">
+        <v-dialog
+          transition="dialog-bottom-transition"
+          max-width="600"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              v-bind="attrs"
+              v-on="on"
+            >From the bottom</v-btn>
+          </template>
+          <template v-slot:default="dialog">
+            <v-card>
+              <v-toolbar
+                color="primary"
+                dark
+              >Opening from the bottom</v-toolbar>
+              <v-card-text>
+                <div class="text-h2 pa-12">Hello world!</div>
+              </v-card-text>
+              <v-card-actions class="justify-end">
+                <v-btn
+                  text
+                  @click="dialog.value = false"
+                >Close</v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </v-col>
+				
+				</v-card>
+			</v-layout>
+		</v-container>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ["info"],
+		name: "OrderCard",
+		data: () => {
+			return {
+				reveal: false,
+                cards: [],
+			};
+		},
+		methods: {
+			
+		},
+	};
+</script>
+<style>
+	.v-card--reveal {
+		bottom: 0;
+		opacity: 1 !important;
+		position: absolute;
+		width: 100%;
+	}
+</style>
--- src/views/Narudzbe.vue
+++ src/views/Narudzbe.vue
@@ -1,5 +1,75 @@
 <template>
+	<div>
+		<div class="loadin" v-if="!load"><loadinga /></div>
+		<v-layout row wrap style>
+			<order-card v-for="n in cards" :key="n.id" :info="n" /><!-- 
+			<meni-card @ondelete="getMeni" v-for="n in cards" :key="n.id" :info="n" /> -->
+		</v-layout>
+	</div>
 </template>
-
 <script>
+	import OrderCard from "../components/OrderCard.vue";
+	import { firebase } from "@/firebase";
+	import { db } from "@/firebase";
+	import store from "@/store.js";
+	import Loadinga from "../components/Loadinga.vue";
+
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user) {
+			// User is signed in.
+
+			store.currentUser = user.email;
+		} else {
+			// User is not signed in.
+
+			store.currentUser = null;
+		}
+	});
+	export default {
+		name: "Narudzbe",
+		components: {
+			OrderCard,
+			Loadinga,
+		},
+		data() {
+			return {
+				dialog: false,
+				store,
+				load: true,
+				cards: [],
+			};
+		},
+		mounted() {
+			this.getOrder();
+		},
+		methods: {
+			loadOn() {
+				let a = this.load;
+				this.load = !a;
+			},
+			getOrder() {
+				this.loadOn();
+				console.log("getorder");
+				db.collection("narudzba")
+					.get()
+					.then((query) => {
+						this.cards = [];
+						query.forEach((doc) => {
+							const dok = doc.data();
+							dok.id = doc.id;
+                            this.cards.push(dok);
+						});
+
+						console.log("final dok", this.cards);
+					});
+				this.loadOn();
+			},
+		},
+	};
 </script>
+
+<style>
+	.row {
+		margin-right: -55px !important;
+	}
+</style>
